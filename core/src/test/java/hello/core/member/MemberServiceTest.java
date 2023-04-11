65d7b24350f845b6ac4df5404f98702586005dfe
lold2424 <lold2424@gmail.com>
2023-04-11 19:01:30 +0900

    20day Object Oriented Principles

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,23 +1,14 @@
 package hello.core.member;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 
 class MemberServiceTest {
+    MemberService memberService;
 
-    MemberService memberService = new MemberServiceImpl();
-
-    @Test
-    void join() {
-        //given
-        Member member = new Member(1L, "memberA", Grade.VIP);
-
-        //when
-        memberService.join(member);
-        Member findMember = memberService.findMember(1L);
-
-        //then
-        Assertions.assertThat(member).isEqualTo(findMember);
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
     }
 }
