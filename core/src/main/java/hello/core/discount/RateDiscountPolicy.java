78c69c4ac1f7876080613953aba6e123c4818e15
lold2424 <lold2424@gmail.com>
2023-04-16 22:24:30 +0900

    23day

--- core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,7 +2,9 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.stereotype.Component;
 
+@Component
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private  int discountPercent = 10;
@@ -12,8 +14,7 @@ public class RateDiscountPolicy implements DiscountPolicy {
         if (member.getGrade() == Grade.VIP) {
             return price * discountPercent / 100;
         } else {
-
+            return 0;
         }
-        return 0;
     }
 }
