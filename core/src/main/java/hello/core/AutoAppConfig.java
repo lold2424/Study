2dd5eb3068bcf9e59bc202aac8355194152ad1b7
lold2424 <lold2424@gmail.com>
2023-04-17 19:13:24 +0900

    23day component Scan

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -8,8 +11,8 @@ import static org.springframework.context.annotation.ComponentScan.*;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 
 public class AutoAppConfig {
-
 }
