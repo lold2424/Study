3018f9d4f85838fbc795abdf5edbe55021215a5d
lold2424 <lold2424@gmail.com>
2023-05-04 16:51:22 +0900

    29day

--- core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,30 +1,21 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SingletonWithPrototypeTest1 {
 
     @Test
-    void prototypeFind() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
-        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
-        prototypeBean1.addCount();
-        assertThat(prototypeBean1.getCount()).isEqualTo(1);
-        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
-        prototypeBean2.addCount();
-        assertThat(prototypeBean2.getCount()).isEqualTo(1);
-    }
-
-    @Test
-    void singletonClientUsePrototype() {
+    void providerTest() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
@@ -33,17 +24,17 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
+
+    @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
@@ -55,7 +46,8 @@ public class SingletonWithPrototypeTest1 {
 
         private int count = 0;
 
-        public void addCount() { count++;
+        public void addCount() {
+            count++;
         }
 
         public int getCount() {
